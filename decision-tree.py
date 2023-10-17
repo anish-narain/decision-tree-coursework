e3f54e8d9c152e51bcccd0e61170e1489933ba20
anish-narain <69715492+anish-narain@users.noreply.github.com>
2023-10-17 23:59:35 +0100

    added code to build decision tree without find_split

--- decision-tree.py
+++ decision-tree.py
@@ -3,25 +3,27 @@ import os
 import matplotlib
 
 def read_dataset(filepath):
-      x = []
-      y_labels = []
-      for line in open(filepath):
-          if line.strip(): 
-              row = line.strip().split("\t")
-              x.append(list(map(float, row[:-1])))
-              y_labels.append(row[-1])
+    x = []
+    y_labels = []
+    for line in open(filepath):
+        if line.strip(): 
+            row = line.strip().split("\t")
+            x.append(list(map(float, row[:-1])))
+            y_labels.append(row[-1])
 
-      [classes, y] = np.unique(y_labels, return_inverse=True)
+    [classes, y] = np.unique(y_labels, return_inverse=True)
 
-      x = np.array(x)
-      y = np.array(y)
-      return (x, y, classes) #return dataset
+    x = np.array(x)
+    y = np.array(y)
+    return (x, y, classes) #return dataset
 
-(x, y, classes) = read_dataset("./wifi_db/clean_dataset.txt")
-print(x, "\n")
-print(y, "\n")
-print(classes, "\n")
-
+"""
+Task 1
+#(x, y, classes) = read_dataset("./wifi_db/clean_dataset.txt")
+#print(x, "\n")
+#print(y, "\n")
+#print(classes, "\n")
+"""
 class Node:
     def __init__(self, attribute, value):
         self.attribute = attribute
@@ -30,8 +32,8 @@ class Node:
         self.right = None
 
 class DecisionTree:
-    def __init__(self, root):
-        self.root = Node(root)
+    def __init__(self, root_attribute, root_value):
+        self.root = Node(root_attribute, root_value)
     
     def visualize_tree(self, node, level=0, prefix="Root: "):
         if node is not None:
@@ -40,25 +42,58 @@ class DecisionTree:
                 self.visualize_tree(node.left, level + 1, "L--- ")
                 self.visualize_tree(node.right, level + 1, "R--- ")
 
-def find_split(training_dataset):
-    return (4, -0.64) #random input
+def find_split(dataset):
+    ...
+
+def split_dataset(training_dataset, split_attribute, split_value):
+    # Extract the attribute column
+    attribute_column = training_dataset[:, split_attribute]
+
+    # Create boolean masks for the left and right datasets
+    left_mask = attribute_column < split_value
+    right_mask = attribute_column >= split_value
+
+    # Use boolean indexing to create the left and right datasets
+    left_dataset = training_dataset[left_mask]
+    right_dataset = training_dataset[right_mask]
+
+    return left_dataset, right_dataset
+
+def reachedLeaf(training_dataset):
+    return np.all(training_dataset[:, -1] == training_dataset[:, -1][0])
+
 
 def decision_tree_learning(training_dataset, depth):
-    split_attribute, split_value = find_split(training_dataset)
-    
+    if reachedLeaf(training_dataset):
+        # Create a leaf node and return it
+        return Node("Leaf", 0.0000), depth
+    else:
+        split_attribute, split_value = find_split(training_dataset)
+        tree_pointer = Node(split_attribute, split_value)
+        left_dataset, right_dataset = split_dataset(training_dataset, split_attribute, split_value)
+        
+        tree_pointer.left, left_depth = decision_tree_learning(left_dataset, depth+1)
+        tree_pointer.right, right_depth = decision_tree_learning(right_dataset, depth+1)
+        return tree_pointer, max(left_depth, right_depth)
+   
+
 
 def main():
-    # Create a simple decision tree
-    tree = DecisionTree("Outlook")
-    tree.root.left = Node("Temperature", "80")
-    tree.root.right = Node("Humidity", "70")
-    tree.root.left.left = Node("Windy", "True")
-    tree.root.left.right = Node("Play Golf", "No")
-    tree.root.right.left = Node("Windy", "False")
-    tree.root.right.right = Node("Play Golf", "Yes")
+    dataset = np.loadtxt("wifi_db/clean_dataset.txt")
+    # Call the decision_tree_learning function to build the decision tree
+    tree, depth = decision_tree_learning(dataset, depth=0)
 
-    # Visualize the tree
-    tree.visualize_tree(tree.root)
+    # You can now traverse the tree and make predictions or explore its structure
+    # For example, let's traverse the leftmost branch until a leaf is reached:
+    current_node = tree
+    while current_node.attribute is not None:
+        print(f"Attribute: {current_node.attribute}, Value: {current_node.value}")
+        current_node = current_node.left
+
+    # Print the final leaf node
+    print(f"Leaf Node Value: {current_node.value}")
+
+
 
 if __name__ == "__main__":
     main()
